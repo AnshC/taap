2b28c13cf9bd4d1d8bb2f8333c58ed3839999329
Ansh Chauhan <unsh@Meghnas-MacBook-Air.local>
2023-04-25 14:42:18 +0530

    initial

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+function Home () {
+    return (
+        <div className="Home">
+            <div className="hero">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,27 @@
+export default function Navbar () {
+    return (
+        <div className="Navbar">
+            <div className="bar">
+                <div className="brand">
+                    <h1>TAAP</h1>
+                </div>
+                <div className="link-section">
+                    <ul className="links">
+                        <li className="link">
+                            Home
+                        </li>
+                        <li className="link">
+                            About
+                        </li>
+                        <li className="link">
+                            Yoga Classes
+                        </li>
+                        <li className="link">
+                            More
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
